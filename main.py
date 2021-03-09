87d5206cb551960b7bbab5c6003dd0a0df5afc32
andrea Lepori <57524583+andrealepori@users.noreply.github.com>
2021-03-09 17:32:50 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,18 @@
+print("Hello, World!");
+print("sono il barista!");
+
+drink_speciale="DigitalVodka"
+print(drink_speciale);
+drink_speciale="senzaVodka"
+print(drink_speciale);
+
+
+nome="Luca";
+eta=28;
+ruolo="barman"
+pub_aperto=False;
+print(nome);
+print(eta);
+print(ruolo);
+print(pub_aperto);
+
