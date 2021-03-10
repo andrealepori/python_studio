57ae9f5bedb1889b7cfbc440a7d469f36f39ac29
andrea Lepori <57524583+andrealepori@users.noreply.github.com>
2021-03-10 13:06:55 +0000

    push

--- main.py
+++ main.py
@@ -1,18 +1,63 @@
-print("Hello, World!");
-print("sono il barista!");
+#print("Hello, World!");
+#print("sono il barista!");
 
-drink_speciale="DigitalVodka"
-print(drink_speciale);
-drink_speciale="senzaVodka"
-print(drink_speciale);
+#drink_speciale="DigitalVodka"
+#print(drink_speciale);
+#drink_speciale="senzaVodka"
+#print(drink_speciale);
 
 
-nome="Luca";
-eta=28;
-ruolo="barman"
-pub_aperto=False;
-print(nome);
-print(eta);
-print(ruolo);
-print(pub_aperto);
+#nome="Luca";
+#eta=28;
+#anno_nascita=1995;
+#ruolo="barman"
+#pub_aperto=False;
+#print(nome);
+#print(eta);
+#print(ruolo);
+#print(pub_aperto);
 
+#print("Assaggia")
+#print(drink_speciale)
+
+
+#stringa_utente=input()
+#print (stringa_utente)
+#print("come ti chiami?")
+#nome_cliente=input()
+#print( "benvenuto")
+#print(nome_cliente)
+#print( "in che anno sei nato?")
+#anno_nascita = input()
+#print("sei nato nel:")
+#print(anno_nascita)
+
+#print("hai a disposizione")
+#drink_disponibili = 5+5
+#print(drink_disponibili) 
+drink_disponibili = 5
+
+drink_disponibili = 5-2
+print(drink_disponibili)
+prezzo_drink= 3.45;
+
+print (drink_disponibili- prezzo_drink)
+
+#Imparare a programmare da zero. Corso di Programmazione Python per Principianti Tutorial Python ITA 23:20
+
+
+#operatori stringa
+
+nome_barman="edoBot";
+
+nome_barman2="ugoBot";
+print (nome_barman +  nome_barman2);
+
+
+a = 3;
+b = "123";
+
+c= a + int(b);
+print (c);
+
+#sono un commento
